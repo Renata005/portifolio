0477147c09620eaf2b34496f75f40ed01e406e5d
Renata005 <renataslopes05@gmail.com>
2025-10-31 16:46:43 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector(".burguer");
-const navbarLinks = document.querySelector(".navbar__links");
-
-button.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-});
